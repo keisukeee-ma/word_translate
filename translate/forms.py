e72bce6f26be3bb30e613a92cfbf845f3e1a632d
EC2 Default User <ec2-user@ip-172-31-9-52.ap-northeast-1.compute.internal>
2022-01-10 15:04:03 +0000

    とりあえず完成

--- translate/forms.py
+++ translate/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class KakikomiForm(forms.Form):
-     word = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'翻訳したい言葉を入れてください。'}))
+     word = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'翻訳したい言葉を入れてください。','class': 'form_field'}),label='')
