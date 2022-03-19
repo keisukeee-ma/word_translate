ab79b550fab03221e568a94187d0d09aec38cfca
EC2 Default User <ec2-user@ip-172-31-9-52.ap-northeast-1.compute.internal>
2022-03-19 10:36:26 +0000

    last18

--- config/settings.py
+++ config/settings.py
@@ -82,14 +82,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'name',
-        'USER': 'user',
-        'PASSWORD': '',
-        'HOST': 'host',
-        'PORT': '',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'name',
+    #     'USER': 'user',
+    #     'PASSWORD': '',
+    #     'HOST': 'host',
+    #     'PORT': '',
+    # }
+    'default': dj_database_url.config()
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
